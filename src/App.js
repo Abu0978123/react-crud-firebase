8d4273c52c81895fd653458c9aae9996f2fd203f
Abu Bakar <abubakar0978123@gmail.com>
2023-06-12 20:44:16 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  return (
+  return ( 
     <div>
       <BrowserRouter>
       <div className="App">
